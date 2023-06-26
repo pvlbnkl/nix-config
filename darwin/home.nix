ec7d35a43099cbdc7a72841e1f78a3ee06b8904e
Pavlo Bunakalia <pblo@tuta.io>
2023-06-26 15:52:44 -0400

    add nix-daemon to shell init

--- darwin/home.nix
+++ darwin/home.nix
@@ -67,6 +67,13 @@
         z = "zathura";
         ncmp = "ncmpcpp";
       };
+      initExtra = ''
+        # Nix
+        if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+        fi
+        # End Nix
+        '';
       };
     starship = {
       enable = true;
