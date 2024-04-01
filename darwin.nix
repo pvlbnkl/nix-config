9a91d06787a2449aa586f6a93ca353d0d66a110e
Pavlo Bunakalia <pblo@tuta.io>
2024-03-31 20:30:49 -0400

    add more binds for managing windows, update default macos settings

--- darwin.nix
+++ darwin.nix
@@ -94,16 +94,22 @@
         KeyRepeat = 1;
         InitialKeyRepeat = 14;
         AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
         NSAutomaticCapitalizationEnabled = false;
         NSAutomaticSpellingCorrectionEnabled = false;
       };
       dock = {
         autohide = true;
+        autohide-delay = 0.2;
+        static-only = true;
       };
       finder = {
-        AppleShowAllExtensions = true;
         _FXShowPosixPathInTitle = true;
+        CreateDesktop = false;
+        FXPreferredViewStyle = "Nlsv";
+        QuitMenuItem = true;
       };
+      screencapture.disable-shadow = true;
     };
     activationScripts.postActivation.text = ''sudo chsh -s ${pkgs.zsh}/bin/zsh''; # declare shell
     stateVersion = 4;
